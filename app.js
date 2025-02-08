68a29ae58cd435eaddf4d9f3df28bca241cd7d05
José Everardo <joseglezsisa@gmail.com>
2025-02-08 14:09:50 -0600

    Agrego funciones de añadir amigo y checar celdas vacias

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigos = [];
+
+
+function agregarAmigo(nombre){
+    let nombreAmigo = document.querySelector(nombre).value;
+    
+    verificarNombreEnCelda();
+}
+
+function verificarNombreEnCelda(){
+    //verificr que la celda no esta vacia
+    if (nombreAmigo == ""){
+        alert("Por favor ingrese un nombre valido");
+    }else{
+        listaDeAmigos.push(nombreAmigo);
+    }
+}
