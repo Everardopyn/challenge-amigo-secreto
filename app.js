8fd94bc4edd6cbe48ec2f20fea9aacd4f3300d6d
José Everardo <joseglezsisa@gmail.com>
2025-02-10 13:43:52 -0600

    Se creo la funcion para que aparezca la lista de amigos y para el sorteo de amigos

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 let listaDeAmigos = [];
 
 
-function agregarAmigo(){
+
+function agregarAmigo(){//se estaba poniendo el id del boton y no del cuadro de texto
     let nombreAmigo = document.querySelector('#amigo').value;
     
 
@@ -13,6 +14,37 @@ function agregarAmigo(){
         listaDeAmigos.push(nombreAmigo);
         document.querySelector("#amigo").value = "";
         console.log(listaDeAmigos);
+        mostrarListaAmigo();
 
     }
 }
+
+function mostrarListaAmigo() {
+
+  let listaAmigos = document.querySelector("#listaAmigos");
+  listaAmigos.textContent = "";
+  let totalLista = 0;
+
+  while (totalLista < listaDeAmigos.length) {
+    //indica la posicion en la lista de amigos, con este se puede cambiar la posicion y mostrar todos los datos de la lista
+    let numeroDeLista = listaDeAmigos[totalLista];
+
+    //forzosamente tienes que poner "li" ya que sino muestra la lista pegada en vertical
+    let listaEnHTML = document.createElement("li");
+    listaEnHTML.textContent = numeroDeLista;
+    listaAmigos.appendChild(listaEnHTML)
+    totalLista++;
+  }
+}
+
+function sortearAmigo(){
+  let totalAmigos = listaDeAmigos.length;
+  let numero = Math.floor(Math.random()* totalAmigos);
+  let numeroSorteado = listaDeAmigos[numero];
+  let mostrar = document.querySelector("#resultado");
+  mostrar.textContent = numeroSorteado;
+
+  
+ console.log(numeroSorteado);
+}
+
